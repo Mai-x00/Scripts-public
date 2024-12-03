21541334607ff3dc95be3e36cfcf9e6a1d99a19e
Mai-x00 <mai.franze@gmail.com>
2024-12-03 21:03:35 +0100

    Simplified "run.ps1"

--- Audio/run.ps1
+++ Audio/run.ps1
@@ -1,13 +1,8 @@
 Add-Type -AssemblyName presentationCore
-$filepath = "$(Get-Location)\sound.wav"
+$filepath = "$($PSScriptRoot)\sound.wav"
 
-$wmplayer = New-Object System.Windows.Media.MediaPlayer
-$wmplayer.Open($filepath)
-
-while (!$wmplayer.NaturalDuration.HasTimeSpan) {
-    Start-Sleep(0.1)
-}
-$duration = $wmplayer.NaturalDuration.TimeSpan.Seconds
+$player = New-Object System.Media.SoundPlayer
+$player.SoundLocation = $filepath
 
 $max_vol = {
     Add-Type -TypeDefinition @'
@@ -70,12 +65,5 @@ $max_vol = {
 }
 # Start Max Volume
 Start-Job -ScriptBlock $max_vol
-#Start-Process powershell.exe -WindowStyle Hidden $max_vol
 
-$wmplayer.Play()
-
-while ($true) {
-    Start-Sleep ($duration + 0.2)
-    $wmplayer.Stop()
-    $wmplayer.Play()
-}
+$player.PlayLooping()
