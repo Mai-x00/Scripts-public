e42f370185ceee0c0886c9feb8f3c3c92eeb8597
Mai-x00 <mai.franze@gmail.com>
2024-12-08 15:48:29 +0100

    Added Wallpaper replacer and updated xmas.zip

--- Xmas/run.ps1
+++ Xmas/run.ps1
@@ -1,8 +1,27 @@
 Add-Type -AssemblyName presentationCore
 $filepath = "$($PSScriptRoot)\sound.wav"
+$wallpaper = "$($PSScriptRoot)\img.jpg"
 
 $player = New-Object System.Media.SoundPlayer
 $player.SoundLocation = $filepath
+
+$change_wallpaper = {
+    Add-Type -TypeDefinition @'
+    using System.Runtime.InteropServices;
+    public class Wallpaper {
+        public const uint SPI_SETDESKWALLPAPER = 0x0014;
+        public const uint SPIF_UPDATEINIFILE = 0x01;
+        public const uint SPIF_SENDWININICHANGE = 0x02;
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
+        public static void SetWallpaper (string path) {
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        }
+    }
+'@
+
+    [Wallpaper]::SetWallpaper($wallpaper)
+}
 
 $max_vol = {
     Add-Type -TypeDefinition @'
@@ -65,6 +84,7 @@ $max_vol = {
 }
 # Start Max Volume
 Start-Job -ScriptBlock $max_vol
+Start-Job -ScriptBlock $change_wallpaper
 
 while ($true) {
     $player.PlaySync()
