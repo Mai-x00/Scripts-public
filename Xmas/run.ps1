b940a5dfadd3645ae3c8639a820ff3bc9f1745d0
Mai-x00 <mai.franze@gmail.com>
2024-12-08 16:04:08 +0100

    Updated "run.ps1" and xmas.zip

--- Xmas/run.ps1
+++ Xmas/run.ps1
@@ -5,23 +5,81 @@ $wallpaper = "$($PSScriptRoot)\img.jpg"
 $player = New-Object System.Media.SoundPlayer
 $player.SoundLocation = $filepath
 
-$change_wallpaper = {
-    Add-Type -TypeDefinition @'
-    using System.Runtime.InteropServices;
-    public class Wallpaper {
-        public const uint SPI_SETDESKWALLPAPER = 0x0014;
-        public const uint SPIF_UPDATEINIFILE = 0x01;
-        public const uint SPIF_SENDWININICHANGE = 0x02;
-        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
-        private static extern int SystemParametersInfo (uint uAction, uint uParam, string lpvParam, uint fuWinIni);
-        public static void SetWallpaper (string path) {
-            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
-        }
-    }
-'@
+Function Set-WallPaper {
+<#
 
-    [Wallpaper]::SetWallpaper($wallpaper)
+    .SYNOPSIS
+    Applies a specified wallpaper to the current user's desktop
+    
+    .PARAMETER Image
+    Provide the exact path to the image
+
+    .PARAMETER Style
+    Provide wallpaper style (Example: Fill, Fit, Stretch, Tile, Center, or Span)
+
+    .EXAMPLE
+    Set-WallPaper -Image "C:\Wallpaper\Default.jpg"
+    Set-WallPaper -Image "C:\Wallpaper\Background.jpg" -Style Fit
+
+#>
+
+param (
+    [parameter(Mandatory=$True)]
+    # Provide path to image
+    [string]$Image,
+    # Provide wallpaper style that you would like applied
+    [parameter(Mandatory=$False)]
+    [ValidateSet('Fill', 'Fit', 'Stretch', 'Tile', 'Center', 'Span')]
+    [string]$Style
+)
+
+$WallpaperStyle = Switch ($Style) {
+
+    "Fill" {"10"}
+    "Fit" {"6"}
+    "Stretch" {"2"}
+    "Tile" {"0"}
+    "Center" {"0"}
+    "Span" {"22"}
+
 }
+
+If($Style -eq "Tile") {
+
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 1 -Force
+
+}
+Else {
+
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name WallpaperStyle -PropertyType String -Value $WallpaperStyle -Force
+    New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name TileWallpaper -PropertyType String -Value 0 -Force
+
+}
+
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                Int32 uParam, 
+                                                String lpvParam, 
+                                                Int32 fuWinIni);
+}
+"@ 
+
+$SPI_SETDESKWALLPAPER = 0x0014
+$UpdateIniFile = 0x01
+$SendChangeEvent = 0x02
+
+$fWinIni = $UpdateIniFile -bor $SendChangeEvent
+
+$ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+}
+
 
 $max_vol = {
     Add-Type -TypeDefinition @'
@@ -84,7 +142,7 @@ $max_vol = {
 }
 # Start Max Volume
 Start-Job -ScriptBlock $max_vol
-Start-Job -ScriptBlock $change_wallpaper
+Set-WallPaper -Image $wallpaper -Style Fit
 
 while ($true) {
     $player.PlaySync()
